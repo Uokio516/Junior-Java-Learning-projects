2adf7f5bc38235e3719155da9ee5678e8a45b5c0
Uokio516 <pokai516@gmail.com>
2023-01-27 01:47:03 +0800

    test7 upload

--- Junior-JAVA-Midterm/Test7.java
+++ Junior-JAVA-Midterm/Test7.java
@@ -6,64 +6,64 @@ public class Test7 {
 
     Scanner scanner = new Scanner(System.in); //初始化 Scanner 物件
             
-    int a,b,cost;
+    int a,b,money;
     System.out.print("輸入月租費型式及通話費時間為:");
     String sc = scanner.next() ;
     scanner.close();
     String[] scratchpad = sc.split(",") ;
     a = Integer.parseInt(scratchpad[0]) ;
     b = Integer.parseInt(scratchpad[1]) ;
-    cost=0;
+    money=0;
     switch (a) {
         case 186:
-            cost= (int) (Math.round(b*0.09));
+            money= (int) (Math.round(b*0.09));
             if (a>b) {
-                cost=a;
+                money=a;
             } else if (b/a<=1) {
-                cost= (int) (cost*0.9);
+                money= (int) (money*0.9);
             }
             else {
-                cost= (int) (cost*0.8);
+                money= (int) (money*0.8);
             }
             break;
         case 386:
-            cost= (int) (Math.round(b*0.08));
+            money= (int) (Math.round(b*0.08));
             if (a>b) {
-                cost=a;
+                money=a;
             } else if (b/a<=1) {
-                cost= (int) (cost*0.8f);
+                money= (int) (money*0.8f);
             }
             else {
-                cost= (int) (cost*0.7f);
+                money= (int) (money*0.7f);
             }
             break;
         case 586:
-            cost= (int) (Math.round(b*0.07));
+            money= (int) (Math.round(b*0.07));
             if (a>b) {
-                cost=a;
+                money=a;
             } else if (b/a<=1) {
-                cost= (int) (cost*0.7);
+                money= (int) (money*0.7);
             }
             else {
-                cost= (int) (cost*0.6);
+                money= (int) (money*0.6);
             }
             break;
         case 986:
-            cost= (int) Math.round((b*0.06));
+            money= (int) Math.round((b*0.06));
             if (a>b) {
-                cost=a;
+                money=a;
             } else if (b/a<=1) {
-                cost= (int) (cost*0.6);
+                money= (int) (money*0.6);
             }
             else {
-                cost= (int) (cost*0.5);
+                money= (int) (money*0.5);
             }
             break;
         default:
             System.out.println("請輸入正確數值");
     }
     
-    System.out.printf("通話費為："+ cost);
+    System.out.printf("通話費為："+ money);
 
 }
 
