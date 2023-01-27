51708c5fa1d1d209b9d392890d1b71971e54d4c9
Uokio516 <pokai516@gmail.com>
2023-01-28 05:29:12 +0800

    RBtree

--- week14/redBlackTree.java
+++ week14/redBlackTree.java
@@ -1,91 +1,105 @@
-public class redBlackTree {
+class RedBlackTree {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
 
+    // 定義節點類別
     class Node {
         int key;
         Node left, right;
-        int color;
-        Node(int d) {
-            key = d;
-            left = right = null;
-            color = 1;
+        boolean color;
+
+        Node(int key) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+            this.color = RED;
         }
     }
-    Node root;
-    void insert(int key) {
-        root = insertRec(root, key);
+
+    private Node root;
+
+    // 新增節點
+    public void insert(int key) {
+        root = insert(root, key);
+        root.color = BLACK;
     }
-    Node insertRec(Node root, int key) {
-        if (root == null) {
-            root = new Node(key);
-            return root;
+
+    // 將新節點插入到樹中
+    private Node insert(Node node, int key) {
+        if (node == null) {
+            return new Node(key);
         }
-        if (key < root.key) {
-            root.left = insertRec(root.left, key);
-        } else if (key > root.key) {
-            root.right = insertRec(root.right, key);
+        if (key < node.key) {
+            node.left = insert(node.left, key);
+        } else {
+            node.right = insert(node.right, key);
         }
-        if (isRed(root.right) && !isRed(root.left)) {
-            root = rotateLeft(root);
+
+        // 如果右子節點是紅色且左子節點是黑色，就對節點進行左旋
+        if (isRed(node.right) && !isRed(node.left)) {
+            node = rotateLeft(node);
         }
-        if (isRed(root.left) && isRed(root.left.left)) {
-            root = rotateRight(root);
+        // 如果左子節點是紅色且左子節點的左子節點也是紅色，就對節點進行右旋
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rotateRight(node);
         }
-        if (isRed(root.left) && isRed(root.right)) {
-            flipColors(root);
+        // 如果左右子節點都是紅色，就對節點進行顏色翻轉
+        if (isRed(node.left) && isRed(node.right)) {
+            flipColors(node);
         }
-        return root;
+
+        return node;
     }
-    boolean isRed(Node node) {
+
+    // 左旋
+    private Node rotateLeft(Node node) {
+        Node right = node.right;
+        node.right = right.left;
+        right.left = node;
+        right.color = node.color;
+        node.color = RED;
+        node.right.color = BLACK;
+        return right;
+        }
+    // 右旋
+    private Node rotateRight(Node node) {
+        Node left = node.left;
+        node.left = left.right;
+        left.right = node;
+        left.color = node.color;
+        node.color = RED;
+        left.right.color = BLACK;
+        return left;
+    }
+
+    // 顏色翻轉
+    private void flipColors(Node node) {
+        node.color = !node.color;
+        node.left.color = !node.left.color;
+        node.right.color = !node.right.color;
+    }
+
+    // 判斷節點顏色
+    private boolean isRed(Node node) {
         if (node == null) {
             return false;
         }
-        return (node.color == 1);
+        return node.color == RED;
     }
-    Node rotateLeft(Node h) {
-        Node x = h.right;
-        h.right = x.left;
-        x.left = h;
-        x.color = h.color;
-        h.color = 1;
-        return x;
-    }
-    Node rotateRight(Node h) {
-        Node x = h.left;
-        h.left = x.right;
-        x.right = h;
-        x.color = h.color;
-        h.color = 1;
-        return x;
-    }
-    void flipColors(Node h) {
-        h.color = 1;
-        h.left.color = 0;
-        h.right.color = 0;
-    }
-    void inorder() {
-        inorderRec(root);
-    }
-    void inorderRec(Node root) {
-        if (root != null) {
-            inorderRec(root.left);
-            System.out.println(root.key);
-            inorderRec(root.right);
-        }
-    }
+
     public static void main(String[] args) {
-        redBlackTree tree = new redBlackTree();
-        tree.insert(2);
-        tree.insert(133);
-        tree.insert(41);
-        tree.insert(222);
-        tree.insert(118);
-        tree.insert(242);
-        tree.insert(68);
-        tree.insert(69);
-        tree.insert(88);
-        tree.insert(82);
-        tree.insert(1);
-        tree.insert(0);
-        tree.inorder();
+        RedBlackTree tree = new RedBlackTree();
+
+        // 新增10個節點
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+        tree.insert(90);
+        tree.insert(100);
+        tree.insert(110);
     }
 }
