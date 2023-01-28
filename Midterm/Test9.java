d4329f48f09b842c77db67748bd8622ea613f952
Uokio516 <pokai516@gmail.com>
2023-01-28 08:19:14 +0800

    Test9 upload

---
+++ Midterm/Test9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("輸入s1為：");
+        String s1 = sc.nextLine();
+        System.out.print("輸入s2為：");
+        String s2 = sc.nextLine();
+        sc.close();
+        if (s2.contains(s1)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
